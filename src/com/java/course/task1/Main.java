ba974bb75864079663f749704926dc59a45656bd
AnnaAvl <86964707+AnnaAvl@users.noreply.github.com>
2021-10-14 18:56:10 +0300

    done task1

--- src/com/java/course/task1/Main.java
+++ src/com/java/course/task1/Main.java
@@ -1,7 +1,47 @@
 package com.java.course.task1;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the size of the matrix");
+        int raz = s.nextInt();
+        int[][] arr = new int[raz][raz];
+        System.out.println("Enter the matrix by lines");
+        for (int i = 0; i < raz; i++) {
+            for (int j = 0; j < raz; j++) {
+                arr[i][j] = s.nextInt();
+//                arr[i][j] = (int) (Math.random() * 10);
+            }
+        }
 
+        print(arr);
+
+        int[] arr_1 = new int[2 * raz - 1];
+        Arrays.setAll(arr_1, a -> (a = 0));
+
+
+        System.out.println();
+
+        for (int i = 0; i < raz; i++) {
+            for (int j = raz - 1; j >= 0; j--) {
+                arr_1[(j + i)] += arr[i][j];
+            }
+        }
+
+        for (int i = 0; i < (2 * raz - 1); i++) {
+            System.out.print(arr_1[i] + " ");
+        }
+    }
+
+    public static void print(int[][] arr) {
+        for (int[] i : arr) {
+            for (int j : i) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
     }
 }
