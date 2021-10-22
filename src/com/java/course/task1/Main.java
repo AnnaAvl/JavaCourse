9f859467202522f79d89f82123eaf58a695bd55e
AnnaAvl <86964707+AnnaAvl@users.noreply.github.com>
2021-10-22 17:42:08 +0300

    fixed

--- src/com/java/course/task1/Main.java
+++ src/com/java/course/task1/Main.java
@@ -3,6 +3,8 @@ package com.java.course.task1;
 import java.util.Arrays;
 import java.util.Scanner;
 
+// Справа - налево, сверху - вниз
+
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
@@ -12,13 +14,26 @@ public class Main {
         System.out.println("Enter the matrix by lines");
         for (int i = 0; i < raz; i++) {
             for (int j = 0; j < raz; j++) {
-                arr[i][j] = s.nextInt();
-//                arr[i][j] = (int) (Math.random() * 10);
+//                arr[i][j] = s.nextInt();
+                arr[i][j] = (int) (Math.random() * 10);
             }
         }
 
         print(arr);
+        int[] arr_s = sum(arr);
+    }
 
+    public static void print(int[][] arr) {
+        for (int[] i : arr) {
+            for (int j : i) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[] sum(int[][]arr){
+        int raz = arr.length;
         int[] arr_1 = new int[2 * raz - 1];
         Arrays.setAll(arr_1, a -> (a = 0));
 
@@ -30,18 +45,10 @@ public class Main {
                 arr_1[(j + i)] += arr[i][j];
             }
         }
-
         for (int i = 0; i < (2 * raz - 1); i++) {
             System.out.print(arr_1[i] + " ");
         }
-    }
 
-    public static void print(int[][] arr) {
-        for (int[] i : arr) {
-            for (int j : i) {
-                System.out.print(j + " ");
-            }
-            System.out.println();
-        }
+        return arr_1;
     }
 }
